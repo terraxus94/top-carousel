9a5932352f27174a26412329b7f69cfd2123072d
terraxus94 <antonio.marusic94@gmail.com>
2021-02-22 21:33:59 +0100

    test

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper');
+
+(function eventHandlers() {
+  const slideContainer = wrapper.querySelector('.slide-container');
+  let slides = slideContainer.querySelectorAll('.carousel-slide');
+})();
